3c19a956611ea6404ad0a184aaadec7b9ac214d7
TiagoSantosDev <up201808919@fe.up.pt>
2022-10-10 17:13:48 +0100

    added reactivex to gradle

--- common/src/main/kotlin/com/tiagosantos/common/ui/base/BaseViewModel.kt
+++ common/src/main/kotlin/com/tiagosantos/common/ui/base/BaseViewModel.kt
@@ -1,33 +1,11 @@
-/*
- * Copyright 2019 http://mobile-dev.pro
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.tiagosantos.common.ui.base
 
 import androidx.lifecycle.LifecycleObserver
 import androidx.lifecycle.ViewModel
-//import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxjava3.
 
-/**
- * Base class for ViewModels
- *
- * http://mobile-dev.pro
- *
- */
 abstract class BaseViewModel : ViewModel(), LifecycleObserver {
-    /*
+
     private var disposable: CompositeDisposable = CompositeDisposable()
 
     val subscriptions: CompositeDisposable
@@ -43,6 +21,6 @@ abstract class BaseViewModel : ViewModel(), LifecycleObserver {
     override fun onCleared() {
         disposable.dispose()
     }
-    */
+
 
 }
