c098c4355a98729515f9fd637a0ade658ee1d859
TiagoSantosDev <up201808919@fe.up.pt>
2022-10-11 16:02:32 +0100

    transition to viewbinding part 2

--- app/src/main/kotlin/com/tiagosantos/crpg_remake/ui/meals/MealsFragment.kt
+++ app/src/main/kotlin/com/tiagosantos/crpg_remake/ui/meals/MealsFragment.kt
@@ -3,6 +3,7 @@ package com.tiagosantos.crpg_remake.ui.meals
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.viewModels
 import com.google.android.material.card.MaterialCardView
 import com.tiagosantos.common.ui.base.BaseFragment
 import com.tiagosantos.common.ui.base.FragmentSettings
@@ -29,18 +30,11 @@ class MealsFragment : BaseFragment<MealsFragmentBinding>(
     val mealSuccessView = view?.findViewById<View>(R.id.meal_choice_success)
     val nothingCheckedWarning = view?.findViewById<View>(R.id.aviso_nenhuma_refeicao_checked)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        val mealsViewModel =
-            ViewModelProvider(activity as AppCompatActivity)[MealsViewModel::class.java]
-    }
-
     @SuppressLint("SetTextI18n", "ResourceAsColor")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        val mealsViewModel = ViewModelProvider(activity as AppCompatActivity).get(MealsViewModel::class.java)
-        ViewModelProvider(activity as AppCompatActivity).get(SharedViewModel::class.java)
         val isLunch = requireArguments().getBoolean("isLunch")
+        val mealsVM: MealsViewModel by viewModels()
 
         val cardList = listOf(
             view.frameOpcaoCarne, view.frameOpcaoPeixe, view.frameOpcaoDieta,
@@ -49,8 +43,8 @@ class MealsFragment : BaseFragment<MealsFragmentBinding>(
 
         fun setChecks(cardList: List<MaterialCardView?>, card: MaterialCardView) {
             if (!card.isChecked) {
-                mealsViewModel.selectedOption = 1
-            } else mealsViewModel.selectedOption = 0
+                mealsVM.selectedOption = 1
+            } else mealsVM.selectedOption = 0
 
             for (s in cardList) {
                 if (s != card) s?.isChecked = false
@@ -73,39 +67,26 @@ class MealsFragment : BaseFragment<MealsFragmentBinding>(
             setChecks(cardList, view.frameOpcaoVegetariano )
         }.also { updateFlagMealChosen() }
 
-        button_confirm?.setOnClickListener {
-            if (mealsViewModel.selectedOption != 0) {
+        view.buttonConfirmMeal.setOnClickListener {
+            if (mealsVM.selectedOption != 0) {
                 mealSuccessView?.visibility = View.VISIBLE
                 mealSuccessView?.bringToFront()
                 nothingCheckedWarning?.visibility = View.GONE
-                mealsViewModel.updateMealChoiceOnLocalStorage(
-                    mealsViewModel.selectedOption,
+                mealsVM.updateMealChoiceOnLocalStorage(
+                    mealsVM.selectedOption,
                     isLunch
                 )
-                button_ok?.setOnClickListener {
+                view.button_ok?.setOnClickListener {
                     mealSuccessView?.visibility = View.GONE
                      }
             } else {
-                nothingCheckedWarning?.visibility = View.VISIBLE
+                view.root.
+                    .visibility = View.VISIBLE
             }
         }
     }
 
-     fun performActionWithVoiceCommand(command: String) {
-        val commandToActionMap = mapOf(
-            "Carne" to cardCarne?.performClick(),
-            "Peixe" to cardPeixe?.performClick(),
-            "Dieta" to cardDieta?.performClick(),
-            "Vegetariano" to cardVeg?.performClick(),
-        )
 
-        val a = commandToActionMap.keys.find {
-            command.contains(it) ||
-                (command.contains(it) && command.contains("Prato"))
-        }
-
-        commandToActionMap[a]
-    }
 
     override fun onInitDataBinding() {
         TODO("Not yet implemented")
