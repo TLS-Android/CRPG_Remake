594823d79b886a2f19d0c460635439dd1a12572a
TiagoSantosDev <up201808919@fe.up.pt>
2022-10-11 15:41:44 +0100

    transition to viewbinding

--- app/src/main/kotlin/com/tiagosantos/crpg_remake/ui/meals/MealsFragment.kt
+++ app/src/main/kotlin/com/tiagosantos/crpg_remake/ui/meals/MealsFragment.kt
@@ -3,39 +3,37 @@ package com.tiagosantos.crpg_remake.ui.meals
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
-import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
 import com.google.android.material.card.MaterialCardView
 import com.tiagosantos.common.ui.base.BaseFragment
 import com.tiagosantos.common.ui.base.FragmentSettings
 import com.tiagosantos.crpg_remake.R
-import com.tiagosantos.crpg_remake.databinding.FragmentMealsBinding
+import com.tiagosantos.crpg_remake.databinding.MealsFragmentBinding
 import java.util.*
 
-class MealsFragment : BaseFragment<FragmentMealsBinding>(
-    layoutId = R.layout.fragment_meals,
+class MealsFragment : BaseFragment<MealsFragmentBinding>(
+    layoutId = R.layout.meals_fragment,
     FragmentSettings(
         appBarTitle = R.string.title_dashboard,
         sharedPreferencesBooleanName = R.string.mealsHasRun.toString(),
     )
 ) {
 
+    private lateinit var view: MealsFragmentBinding
     private var flagMealChosen = false
     val buttonList = listOf(
         true, true, true
     )
 
-    private val cardCarne: MaterialCardView? = view?.findViewById(R.id.frame_opcao_carne)
-    private val cardPeixe: MaterialCardView? = view?.findViewById(R.id.frame_opcao_peixe)
-    private val cardDieta: MaterialCardView? = view?.findViewById(R.id.frame_opcao_dieta)
-    private val cardVeg: MaterialCardView? = view?.findViewById(R.id.frame_opcao_vegetariano)
+    val button_confirm = view?.findViewById<View>(R.id.button_confirm_meal)
+    val button_ok = view?.findViewById<View>(R.id.button_ok)
+    val mealSuccessView = view?.findViewById<View>(R.id.meal_choice_success)
+    val nothingCheckedWarning = view?.findViewById<View>(R.id.aviso_nenhuma_refeicao_checked)
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mealsViewModel =
             ViewModelProvider(activity as AppCompatActivity)[MealsViewModel::class.java]
     }
-
 
     @SuppressLint("SetTextI18n", "ResourceAsColor")
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -45,7 +43,8 @@ class MealsFragment : BaseFragment<FragmentMealsBinding>(
         val isLunch = requireArguments().getBoolean("isLunch")
 
         val cardList = listOf(
-            cardCarne, cardPeixe, cardDieta, cardVeg
+            view.frameOpcaoCarne, view.frameOpcaoPeixe, view.frameOpcaoDieta,
+                view.frameOpcaoVegetariano
         )
 
         fun setChecks(cardList: List<MaterialCardView?>, card: MaterialCardView) {
@@ -58,27 +57,23 @@ class MealsFragment : BaseFragment<FragmentMealsBinding>(
             }
         }
 
-        cardCarne?.setOnClickListener {
-            setChecks(cardList, cardCarne)
+        view.frameOpcaoCarne.setOnClickListener {
+            setChecks(cardList,  view.frameOpcaoCarne)
         }.also { updateFlagMealChosen() }
 
-        cardPeixe?.setOnClickListener {
-            setChecks(cardList, cardPeixe)
+        view.frameOpcaoPeixe.setOnClickListener {
+            setChecks(cardList,  view.frameOpcaoPeixe)
         }.also { updateFlagMealChosen() }
 
-        cardDieta?.setOnClickListener {
-            setChecks(cardList, cardDieta)
+        view.frameOpcaoDieta.setOnClickListener {
+            setChecks(cardList,  view.frameOpcaoPeixe)
         }.also { updateFlagMealChosen() }
 
-        cardVeg?.setOnClickListener {
-            setChecks(cardList, cardVeg)
+        view.frameOpcaoVegetariano.setOnClickListener {
+            setChecks(cardList, view.frameOpcaoVegetariano )
         }.also { updateFlagMealChosen() }
 
-        val mealSuccessView = view?.findViewById<View>(R.id.meal_choice_success)
-        val nothingCheckedWarning = view?.findViewById<View>(R.id.aviso_nenhuma_refeicao_checked)
-
-
-        button_confirm_meal.setOnClickListener {
+        button_confirm?.setOnClickListener {
             if (mealsViewModel.selectedOption != 0) {
                 mealSuccessView?.visibility = View.VISIBLE
                 mealSuccessView?.bringToFront()
@@ -87,13 +82,9 @@ class MealsFragment : BaseFragment<FragmentMealsBinding>(
                     mealsViewModel.selectedOption,
                     isLunch
                 )
-                button_ok.setOnClickListener {
+                button_ok?.setOnClickListener {
                     mealSuccessView?.visibility = View.GONE
-                    handler.removeCallbacksAndMessages(null)
-                    if (handler.hasMessages(0)) {
-                        handler.removeCallbacks(runnable)
-                    }
-                }
+                     }
             } else {
                 nothingCheckedWarning?.visibility = View.VISIBLE
             }
@@ -127,5 +118,10 @@ class MealsFragment : BaseFragment<FragmentMealsBinding>(
     private fun updateFlagMealChosen() {flagMealChosen = !flagMealChosen}
 
 }
+
+//handler.removeCallbacksAndMessages(null)
+//                    if (handler.hasMessages(0)) {
+//                        handler.removeCallbacks(runnable)
+//                    }
 
 
