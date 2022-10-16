d5e4119db2b0d2082d50024c71f5b62553de8f45
Tiago <ltiagosantos.ts@gmail.com>
2022-10-17 00:26:12 +0100

    minor fixes

--- app/src/main/kotlin/com/tiagosantos/crpg_remake/ui/meditation/MeditationMediaPlayerFragment.kt
+++ app/src/main/kotlin/com/tiagosantos/crpg_remake/ui/meditation/MeditationMediaPlayerFragment.kt
@@ -3,30 +3,24 @@ package com.tiagosantos.crpg_remake.ui.meditation
 import android.graphics.Color
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.plataforma.crpg.R
-import com.plataforma.crpg.databinding.FragmentMeditationMediaPlayerBinding
-import com.plataforma.crpg.ui.MainActivity
-import kotlinx.android.synthetic.main.custom_player_control_view.*
-import kotlinx.android.synthetic.main.fragment_meditation_media_player.*
-import net.gotev.speech.GoogleVoiceTypingDisabledException
-import net.gotev.speech.Speech
-import net.gotev.speech.SpeechDelegate
-import net.gotev.speech.SpeechRecognitionNotAvailable
-
+import com.tiagosantos.crpg_remake.R
 
 class MeditationMediaPlayerFragment : Fragment(){
+
+    private val medViewModel: MeditationViewModel by viewModels()
+    private val player = ExoPlayer.Builder(requireContext()).build()
 
     companion object {
         fun newInstance() = MeditationMediaPlayerFragment()
@@ -38,77 +32,42 @@ class MeditationMediaPlayerFragment : Fragment(){
     ): View {
         val binding = FragmentMeditationMediaPlayerBinding.inflate(layoutInflater)
         return binding.root
-        //return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         (activity as AppCompatActivity).supportActionBar?.title = "REPRODUZIR ÁUDIO"
-        val medViewModel = ViewModelProvider(activity as AppCompatActivity).get(MeditationViewModel::class.java)
-
-        println(">Sel mood: " + medViewModel.selectedMood)
         medViewModel.getValue()
         text_selected_mood.text = medViewModel.selectedMood
 
-        when(medViewModel.selectedMood){
-            "RELAXADO" -> {
-                mood_color.setBackgroundColor(Color.parseColor("#00BBF2"))
+        setupPlayer(player)
+
+        with(mood_color){
+            when(medViewModel.selectedMood){
+                "RELAXADO" -> this.setBackgroundColor(android.graphics.Color.parseColor("#00BBF2"))
+                "FELIZ" -> this.setBackgroundColor(android.graphics.Color.parseColor("#87B700"))
+                "SONOLENTO" -> this.setBackgroundColor(android.graphics.Color.parseColor("#FBC02D"))
+                "CONFIANTE" -> this.setBackgroundColor(android.graphics.Color.parseColor("#57A8D8"))
+                "QUERIDO" -> this.setBackgroundColor(android.graphics.Color.parseColor("#AA00FF"))
+                "MENTE SÃ" -> this.setBackgroundColor(android.graphics.Color.parseColor("#57A8D8"))
             }
-            "FELIZ" -> {
-                mood_color.setBackgroundColor(Color.parseColor("#87B700"))
-            }
-            "SONOLENTO" -> {
-                mood_color.setBackgroundColor(Color.parseColor("#FBC02D"))
-            }
-            "CONFIANTE" -> {
-                mood_color.setBackgroundColor(Color.parseColor("#57A8D8"))
-            }
-            "QUERIDO" -> {
-                mood_color.setBackgroundColor(Color.parseColor("#AA00FF"))
-            }
-            "MENTE SÃ" -> {
-                mood_color.setBackgroundColor(Color.parseColor("#57A8D8"))
-            }
+
         }
-
-        val uri: Uri = Uri.parse("android.resource://" + context?.packageName.toString()
-                + "/raw/meditation_sound")
-        val player = SimpleExoPlayer.Builder(requireContext()).build()
-
-        player_view.player = player
-        val mediaItem: MediaItem = MediaItem.fromUri(uri)
-        player.setMediaItem(mediaItem)
-
-        player.prepare()
-        player.play()
 
         button_return_meditation.setOnClickListener {
             player.stop()
             val fragment: Fragment = MeditationFragment()
             val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
             val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-            fragmentTransaction.replace(R.id.nav_host_fragment, fragment)
+            fragmentTransaction.replace(R.id.nav_host_fragment_activity_main, fragment)
             fragmentManager.popBackStack()
             fragmentTransaction.addToBackStack(null)
             fragmentTransaction.commit()
         }
 
         showBackButton()
-        val onBackPressedCallback = object : OnBackPressedCallback(true) {
-            override fun handleOnBackPressed() {
-                // Handle the back button event
-                player.stop()
-                val fragment: Fragment = MeditationFragment()
-                val fragmentManager: FragmentManager = requireActivity().supportFragmentManager
-                val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
-                fragmentTransaction.replace(R.id.nav_host_fragment, fragment)
-                fragmentManager.popBackStack()
-                fragmentTransaction.addToBackStack(null)
-                fragmentTransaction.commit()
-            }
-        }
-        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, onBackPressedCallback)
     }
+
 
 
     fun performActionWithVoiceCommand(command: String){
@@ -121,59 +80,4 @@ class MeditationMediaPlayerFragment : Fragment(){
         }
     }
 
-
-    private fun startVoiceRecognition(){
-        Speech.init(context)
-        try {
-            // you must have android.permission.RECORD_AUDIO granted at this point
-            Speech.getInstance().startListening(object : SpeechDelegate {
-                override fun onStartOfSpeech() {
-                    Log.i("speech", "speech recognition is now active")
-                }
-
-                override fun onSpeechRmsChanged(value: Float) {
-                    Log.d("speech", "rms is now: $value")
-                }
-
-                override fun onSpeechPartialResults(results: List<String>) {
-                    val str = StringBuilder()
-                    for (res in results) {
-                        str.append(res).append(" ")
-                    }
-                    Log.i("speech", "partial result: " + str.toString().trim { it <= ' ' })
-                }
-
-                override fun onSpeechResult(result: String) {
-                    performActionWithVoiceCommand(result)
-                    Log.i("speech", "result: $result")
-                    //println("on Speech Result")
-                }
-            })
-        } catch (exc: SpeechRecognitionNotAvailable) {
-            Log.e("speech", "Speech recognition is not available on this device!")
-            // You can prompt the user if he wants to install Google App to have
-            // speech recognition, and then you can simply call:
-            //
-            // SpeechUtil.redirectUserToGoogleAppOnPlayStore(this);
-            //
-            // to redirect the user to the Google App page on Play Store
-        } catch (exc: GoogleVoiceTypingDisabledException) {
-            Log.e("speech", "Google voice typing must be enabled!")
-        }
-
-    }
-
-    private fun showBackButton() {
-        if (activity is MainActivity) {
-            (activity as MainActivity?)?.supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        }
-    }
-
 }
-//context?.resources?.getColor(R.color.material_blue_200)?.let { mood_color.setColorFilter(it) }
-//
-// getResources().openRawResource(R.raw.meditation_sound)
-//        //val filePath = "raw/meditation_sound.mp3"
-//
-// player.setVideoSurface(null)
-// player.clearVideoSurface()
--- app/src/main/kotlin/com/tiagosantos/crpg_remake/ui/meditation/MeditationViewModel.kt
+++ app/src/main/kotlin/com/tiagosantos/crpg_remake/ui/meditation/MeditationViewModel.kt
@@ -2,7 +2,10 @@ package com.tiagosantos.crpg_remake.ui.meditation
 
 import android.annotation.SuppressLint
 import android.app.Application
+import android.net.Uri
 import androidx.lifecycle.AndroidViewModel
+import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 import com.tiagosantos.common.ui.utils.Constants.EMPTY_STRING
 
 @SuppressLint("StaticFieldLeak")
@@ -10,9 +13,17 @@ class MeditationViewModel(application: Application) : AndroidViewModel(applicati
 
     private val context = application.applicationContext
     var selectedMood = EMPTY_STRING
-    
-    fun getValue(){
-        println(">Selected mood no VM:$selectedMood")
+
+    private fun setupPlayer(player: ExoPlayer, view: FragmentMeditationMediaPlayerBinding) {
+        val uri: Uri = Uri.parse("android.resource://" + context?.packageName.toString()
+                + "/raw/meditation_sound")
+
+        player_view.player = player
+        val mediaItem: MediaItem = MediaItem.fromUri(uri)
+        player.setMediaItem(mediaItem)
+
+        player.prepare()
+        player.play()
     }
 
 }
