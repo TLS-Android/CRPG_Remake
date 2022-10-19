5d8b119fa41953f729fae509befed1d462398682
TiagoSantosDev <up201808919@fe.up.pt>
2022-10-19 22:06:55 +0100

    refactored code

--- app/src/main/kotlin/com/tiagosantos/crpg_remake/ui/agenda/AgendaRepository.kt
+++ app/src/main/kotlin/com/tiagosantos/crpg_remake/ui/agenda/AgendaRepository.kt
@@ -3,7 +3,6 @@ package com.tiagosantos.crpg_remake.ui.agenda
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import com.tiagosantos.common.ui.model.Event
-import com.tiagosantos.common.ui.model.EventType
 import com.tiagosantos.common.ui.utils.Constants
 import java.io.File
 import java.io.FileReader
@@ -14,26 +13,6 @@ object AgendaRepository {
     //private lateinit var database: DatabaseReference
     // create 2 fixed events for lunch and dinner
     val gson = Gson()
-
-    var lunchEvent = Event(
-        "Almoço", "Clicar para escolher refeição", EventType.MEAL,
-        "1200", "1300",
-        "", "", "", false, 0
-    )
-    var dinnerEvent = Event(
-        "Jantar", "Clicar para escolher refeição",
-        EventType.MEAL, "2000", "2100",
-        "", "", "", false, 0
-    )
-
-    val fileContent = """[{"title": "ALMOÇO","info":"test","type": "MEAL", 
-            |"start_time": "1130","end_time": "1230","date": "2021-03-17"},
-            |{"title": "JANTAR","info":"test","type":"MEAL", 
-            |"start_time": "2000","end_time": "2100","date": "2021-03-17"},
-            |{"title": "TRANSPORTE","info":"test","type": "TRANSPORT", 
-            |"start_time": "0830","end_time": "1330","date": "2021-03-17"},
-            |{"title": "Actividade","info":"Sala 12","type": "ACTIVITY",
-            | "start_time": "0830","end_time": "1330","date": "2021-03-17"}]""".trimMargin()
 
     fun initializeDbRef() {
         database = Firebase.database.reference
--- app/src/main/kotlin/com/tiagosantos/crpg_remake/ui/agenda/SharedViewModel.kt
+++ app/src/main/kotlin/com/tiagosantos/crpg_remake/ui/agenda/SharedViewModel.kt
@@ -2,10 +2,32 @@ package com.tiagosantos.crpg_remake.ui.agenda
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import com.tiagosantos.common.ui.model.Event
+import com.tiagosantos.common.ui.model.EventType
 import com.tiagosantos.common.ui.utils.Constants.EMPTY_STRING
 
 class SharedViewModel(
     application: Application
 ) : AndroidViewModel(application) {
     var selectedDate = EMPTY_STRING
+
+    var lunchEvent = Event(
+        "Almoço", "Clicar para escolher refeição", EventType.MEAL,
+        "1200", "1300",
+        "", "", "", false, 0
+    )
+    var dinnerEvent = Event(
+        "Jantar", "Clicar para escolher refeição",
+        EventType.MEAL, "2000", "2100",
+        "", "", "", false, 0
+    )
+
+    val fileContent = """[{"title": "ALMOÇO","info":"test","type": "MEAL", 
+            |"start_time": "1130","end_time": "1230","date": "2021-03-17"},
+            |{"title": "JANTAR","info":"test","type":"MEAL", 
+            |"start_time": "2000","end_time": "2100","date": "2021-03-17"},
+            |{"title": "TRANSPORTE","info":"test","type": "TRANSPORT", 
+            |"start_time": "0830","end_time": "1330","date": "2021-03-17"},
+            |{"title": "Actividade","info":"Sala 12","type": "ACTIVITY",
+            | "start_time": "0830","end_time": "1330","date": "2021-03-17"}]""".trimMargin()
 }
