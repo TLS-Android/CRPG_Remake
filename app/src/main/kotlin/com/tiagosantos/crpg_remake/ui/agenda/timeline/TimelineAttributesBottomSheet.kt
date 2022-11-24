f0ed26c2419b86eaafa7eb512150bd743c8f7814
Tiago <ltiagosantos.ts@gmail.com>
2022-11-24 16:22:36 +0000

    more refactoring

--- app/src/main/kotlin/com/tiagosantos/crpg_remake/ui/agenda/timeline/TimelineAttributesBottomSheet.kt
+++ app/src/main/kotlin/com/tiagosantos/crpg_remake/ui/agenda/timeline/TimelineAttributesBottomSheet.kt
@@ -44,7 +44,7 @@ class TimelineAttributesBottomSheet : RoundedCornerBottomSheet() {
         super.onStart()
 
         if (dialog != null) {
-            val bottomSheet = dialog!!.findViewById<View>(R.id.design_bottom_sheet)
+            val bottomSheet = dialog!!.findViewById<View>()
             bottomSheet.layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
         }
 
