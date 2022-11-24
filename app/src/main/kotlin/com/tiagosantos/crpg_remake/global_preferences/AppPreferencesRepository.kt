f1cb185694eed9b21d42a67fdb48cda47c41e5e1
Tiago <ltiagosantos.ts@gmail.com>
2022-11-24 14:36:43 +0000

    removed boilerplate code

--- app/src/main/kotlin/com/tiagosantos/crpg_remake/global_preferences/AppPreferencesRepository.kt
+++ app/src/main/kotlin/com/tiagosantos/crpg_remake/global_preferences/AppPreferencesRepository.kt
@@ -5,24 +5,22 @@ import android.content.SharedPreferences
 import com.tiagosantos.common.ui.utils.Constants.MODALITY
 
 class AppPreferencesRepository(
-    private val ctx: Context
+    ctx: Context
 ) {
     private lateinit var appPreferences: AppPreferencesStorage
     private fun saveAppPreferences(appPreferences: AppPreferences) {}
 
-    private val literalValue = listOf(
-        "meditationHasRun", "notesHasRun", "notesTextHasRun",
+    private val literalValue = listOf("meditationHasRun", "notesHasRun", "notesTextHasRun",
         "selectionHasRun", "remindersHasRun", "agendaHasRun"
     )
 
-    fun resetAppPreferences() {
-        val sharedPreferences = ctx.getSharedPreferences(MODALITY, Context.MODE_PRIVATE).apply {
-            for (i in 1..literalValue.size)  edit().putBoolean(literalValue[i], false).apply()
-        }
-    }
-
-    val sharedPreferences: SharedPreferences? = ctx.getSharedPreferences(
+    private val sharedPreferences: SharedPreferences? = ctx.getSharedPreferences(
         MODALITY,
         Context.MODE_PRIVATE
     )
+
+    fun resetAppPreferences(appPreferences: AppPreferences) = sharedPreferences!!.apply {
+        for (i in 1..literalValue.size)  edit().putBoolean(literalValue[i], false).apply()
+    }
+
 }
--- app/src/main/kotlin/com/tiagosantos/crpg_remake/global_preferences/AppPreferencesStorage.kt
+++ app/src/main/kotlin/com/tiagosantos/crpg_remake/global_preferences/AppPreferencesStorage.kt
@@ -3,4 +3,5 @@ package com.tiagosantos.crpg_remake.global_preferences
 interface AppPreferencesStorage {
     fun <T> getAppPreferences(transformation: (AppPreferencesRealm) -> T): T
     suspend fun saveAppPreferences(appPreferencesRealm: AppPreferencesRealm)
+    suspend fun resetAppPreferences(appPreferencesRealm: AppPreferencesRealm)
 }
