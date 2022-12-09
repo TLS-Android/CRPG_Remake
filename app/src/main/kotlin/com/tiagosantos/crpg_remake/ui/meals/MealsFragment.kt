40a9b0004cab6cde1dc9e7e8eb6e0268854e8dad
Tiago <ltiagosantos.ts@gmail.com>
2022-12-09 00:28:45 +0000

    fixed issue with unresolved resources

--- app/src/main/kotlin/com/tiagosantos/crpg_remake/ui/meals/MealsFragment.kt
+++ app/src/main/kotlin/com/tiagosantos/crpg_remake/ui/meals/MealsFragment.kt
@@ -3,7 +3,6 @@ package com.tiagosantos.crpg_remake.ui.meals
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
-import android.widget.FrameLayout
 import androidx.fragment.app.viewModels
 import com.google.android.material.card.MaterialCardView
 import com.tiagosantos.access.modal.settings.SRSettings
