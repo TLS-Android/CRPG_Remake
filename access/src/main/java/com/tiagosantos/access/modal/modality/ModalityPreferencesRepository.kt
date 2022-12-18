b6e877048edaa0969d1eb477e7e77811e6f3ba9f
Tiago <ltiagosantos.ts@gmail.com>
2022-12-18 23:28:19 +0000

    Boarding activity now shows dialogs sequentially and not simultaneously

--- access/src/main/java/com/tiagosantos/access/modal/modality/ModalityPreferencesRepository.kt
+++ access/src/main/java/com/tiagosantos/access/modal/modality/ModalityPreferencesRepository.kt
@@ -1,10 +1,7 @@
 package com.tiagosantos.access.modal.modality
 
-import android.app.Activity
-import android.app.Application
 import android.content.Context
 import android.content.SharedPreferences
-import androidx.appcompat.app.AlertDialog
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.tiagosantos.common.ui.utils.Constants.MODALITY
 import com.tiagosantos.common.ui.utils.Constants.PERMITIR
@@ -15,8 +12,9 @@ import com.tiagosantos.common.ui.utils.Constants.TTS
 class ModalityPreferencesRepository(
     private val ctx: Context,
 ) {
-    fun requestMultiModalityOptions() {
+    var alert = 0
 
+    fun requestMultiModalityOptions() {
         println("requestMultiModalityOptions")
 
         val editor = ctx.getSharedPreferences(
@@ -29,14 +27,6 @@ class ModalityPreferencesRepository(
             "A aplicação possui uma voz virtual que poder dar-lhe indicações de como" +
                 "utilizar a plataforma. Prima o botão \"Permitir\" para ativar esta funcionalidade.",
             TTS,
-            editor
-        )
-
-        launchAlertDialog(
-            "Permitir Comandos de Voz",
-            "A aplicação pode ser usada utilizando " +
-                "utilizar a plataforma. Prima o botão \"Permitir\" para ativar esta funcionalidade.",
-            SR,
             editor
         )
     }
@@ -52,9 +42,29 @@ class ModalityPreferencesRepository(
             .setMessage(message)
             .setPositiveButton(PERMITIR) { _, _ ->
                 editor.putBoolean(putBoolean, true)
+                if (alert == 0) {
+                    launchAlertDialog(
+                        "Permitir Comandos de Voz",
+                        "A aplicação pode ser usada utilizando " +
+                                "utilizar a plataforma. Prima o botão \"Permitir\" para ativar esta funcionalidade.",
+                        SR,
+                        editor
+                    )
+                }
+                alert = 1
             }
             .setNegativeButton(RECUSAR) { _, _ ->
                 editor.putBoolean(putBoolean, false)
+                if (alert == 0) {
+                    launchAlertDialog(
+                        "Permitir Comandos de Voz",
+                        "A aplicação pode ser usada utilizando " +
+                                "utilizar a plataforma. Prima o botão \"Permitir\" para ativar esta funcionalidade.",
+                        SR,
+                        editor
+                    )
+                }
+                alert = 1
             }.show()
     }
 }
