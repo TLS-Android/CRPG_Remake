e8aa47b84c528a9e0d3bb1a59f1a238e2d142be1
Tiago <ltiagosantos.ts@gmail.com>
2022-12-23 15:44:57 +0000

    adding new splash screen using new Android API

--- app/src/main/kotlin/com/tiagosantos/crpg_remake/BoardingActivity.kt
+++ app/src/main/kotlin/com/tiagosantos/crpg_remake/BoardingActivity.kt
@@ -3,7 +3,9 @@ package com.tiagosantos.crpg_remake
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.tiagosantos.access.modal.modality.ModalityPreferencesRepository
+import com.tiagosantos.crpg_remake.databinding.SplashScreenBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -15,11 +17,18 @@ class BoardingActivity : Activity() {
 
     private lateinit var prefRepo: ModalityPreferencesRepository
 
+    private lateinit var binding : SplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
 
-        println("BoardingActivity")
+        //val splashScreen = installSplashScreen()
+
+
+
+
+
 
         prefRepo = ModalityPreferencesRepository(this).apply {
             requestMultiModalityOptions()
