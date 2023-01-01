eb398c5c604a6b52a2be9979c976cc22f9c392c4
Tiago <ltiagosantos.ts@gmail.com>
2023-01-01 20:35:13 +0000

    created Meal Choice in order to improve readability of the code

--- common/src/main/kotlin/com/tiagosantos/common/ui/model/MealChoice.kt
+++ common/src/main/kotlin/com/tiagosantos/common/ui/model/MealChoice.kt
@@ -1,12 +1,31 @@
 package com.tiagosantos.common.ui.model
 
+import com.google.gson.annotations.SerializedName
+
 data class MealChoice(
-    var chosen_meal: String? = "",
-    var isLunch: Boolean? = true,
-    var meal_int: Int? = 1
+    var chosenMealType: ChosenMealType? = ChosenMealType.MEAT,
+    var mealType: MealType? = MealType.LUNCH,
 ) {
     override fun toString(): String {
-        return "chosen_meal: ${this.chosen_meal}, isLunch: ${this.isLunch}," +
-                "meal_Int: ${this.meal_int} \\n "
+        return "chosenMealType: ${this.chosenMealType}, mealType: ${this.mealType} \\n "
     }
 }
+
+enum class MealType {
+    @SerializedName("Lunch")
+    LUNCH,
+    @SerializedName("Dinner")
+    DINNER,
+}
+
+enum class ChosenMealType {
+    @SerializedName("Meat")
+    MEAT,
+    @SerializedName("Fish")
+    FISH,
+    @SerializedName("Veg")
+    VEG,
+    @SerializedName("Diet")
+    DIET,
+}
+
