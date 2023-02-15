f4944593763bb00c32ba89cca90cdd5037a0c928
Tiago <ltiagosantos.ts@gmail.com>
2023-02-15 15:31:00 +0000

    delete boilerplate and packages not in use

--- app/src/main/kotlin/com/tiagosantos/crpg_remake/global_preferences/AppPreferences.kt
+++
@@ -1,6 +0,0 @@
-package com.tiagosantos.crpg_remake.global_preferences
-data class AppPreferences(
-    val language: String,
-    val speechRecognition: Boolean,
-    val voiceAssistant: Boolean,
-)
--- app/src/main/kotlin/com/tiagosantos/crpg_remake/global_preferences/AppPreferencesStorage.kt
+++
@@ -1,7 +0,0 @@
-package com.tiagosantos.crpg_remake.global_preferences
-
-interface AppPreferencesStorage {
-    fun <T> getAppPreferences(transformation: (AppPreferencesRealm) -> T): T
-    suspend fun saveAppPreferences(appPreferencesRealm: AppPreferencesRealm)
-    suspend fun resetAppPreferences(appPreferencesRealm: AppPreferencesRealm)
-}
