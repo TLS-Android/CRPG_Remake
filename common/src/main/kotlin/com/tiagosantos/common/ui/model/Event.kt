a8e236dfcad9ac35cd2b5892e4830f66d77422e4
Tiago <ltiagosantos.ts@gmail.com>
2023-02-25 01:05:39 +0000

    updated dependencies

--- common/src/main/kotlin/com/tiagosantos/common/ui/model/Event.kt
+++ common/src/main/kotlin/com/tiagosantos/common/ui/model/Event.kt
@@ -17,6 +17,14 @@ data class Event(
     }
 }
 
+/**
+ * enums are mostly used as constants that relate to each other.
+ * They can be paired with some parent functions, as well.
+ *
+ * Sealed classes are similar to enums but allow more customizations.
+ * As described, they’re a mix between enums and abstract classes.
+ */
+
 enum class EventType {
     @SerializedName("ACTIVITY")
     ACTIVITY,
